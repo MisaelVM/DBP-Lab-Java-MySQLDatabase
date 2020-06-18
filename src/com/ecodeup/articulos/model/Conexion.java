1482edf3e455e5a1bc6b5471e32061c1109a446a
Misael Svante Vílchez Molina <mvilchezmo@unsa.edu.pe>
2020-06-18 18:22:40 -0500

    commit problemas solucionados

--- src/com/ecodeup/articulos/model/Conexion.java
+++ src/com/ecodeup/articulos/model/Conexion.java
@@ -24,7 +24,7 @@ public class Conexion {
 	public void conectar() throws SQLException {
         if (jdbcConnection == null || jdbcConnection.isClosed()) {
             try {
-                Class.forName("com.mysql.jdbc.Driver");
+                Class.forName("com.mysql.cj.jdbc.Driver");
             } catch (ClassNotFoundException e) {
                 throw new SQLException(e);
             }
